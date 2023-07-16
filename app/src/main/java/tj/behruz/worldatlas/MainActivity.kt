e93c56775d84e7bc3e26da3102e9690ceb73f743
bahromzodabehruz0220 <122207192+bahromzodabehruz0220@users.noreply.github.com>
2023-07-16 13:29:52 -0700

    main feature was implemented

--- app/src/main/java/tj/behruz/worldatlas/MainActivity.kt
+++ app/src/main/java/tj/behruz/worldatlas/MainActivity.kt
@@ -2,11 +2,24 @@ package tj.behruz.worldatlas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import tj.behruz.worldatlas.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Base_Theme_WorldAtlas)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        // Find reference to bottom navigation view
+        // Hook your navigation controller to bottom navigation view
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
